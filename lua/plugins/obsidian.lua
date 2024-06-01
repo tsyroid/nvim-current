ed67bc793f28fa7d2a7d9881b53e5025203e647d
Tom Syroid <tsyroid@gmail.com>
2024-06-01 09:19:38 -0700

    (Add) personalized obsidian plugin

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,157 @@
+return {
+  'epwalsh/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  lazy = false,
+  ft = 'markdown',
+  dependencies = {
+    -- Required.
+    'nvim-lua/plenary.nvim',
+    'hrsh7th/nvim-cmp',
+    'nvim-telescope/telescope.nvim',
+    'nvim-treesitter',
+  },
+  opts = {
+
+    workspaces = {
+      {
+        name = 'KB',
+        path = '~/KB/',
+      },
+    },
+    {
+      name = 'no-vault',
+      path = function()
+        return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+      end,
+      overrides = {
+        notes_subdir = vim.NIL, -- have to use 'vim.NIL' instead of 'nil'
+        new_notes_location = 'current_dir',
+        templates = {
+          folder = vim.NIL,
+        },
+        disable_frontmatter = true,
+      },
+    },
+
+    notes_subdir = '@INBOX',
+
+    daily_notes = {
+      folder = 'Daily Notes',
+      date_format = 'Notes-%Y%m%d',
+      time_format = '%H:%M',
+      template = 'daily-notes.md',
+    },
+
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+    },
+
+    -- Optional, for templates (see below).
+    templates = {
+      folder = 'Templates',
+      date_format = '%Y-%m-%d',
+      time_format = '%H:%M',
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
+    },
+
+    mappings = {
+      -- toggle checkboxes
+      ['<leader>oc'] = {
+        action = function()
+          return require('obsidian').util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- smart action: follow line or toggle_checkbox
+      ['<cr>'] = {
+        action = function()
+          return require('obsidian').util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+      -- move file in current buffer to KB/@INBOX
+      -- ['<leader>om'] = {
+      --   ":!mv '%:p' /Users/taz/KB/@INBOX<cr>:bd<cr>",
+      --   desc = 'Move file in current buffer to KB/@INBOX',
+      --   mode = 'n',
+      -- },
+    },
+
+    new_notes_location = 'notes_subdir',
+
+    -- customize how note IDs are generated given an optional title
+    note_id_func = function(title)
+      local suffix = ''
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. '-' .. suffix
+    end,
+
+    -- Optional, customize how note file names are generated given the ID, target directory, and title.
+    ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+    ---@return string|obsidian.Path The full path to the new note.
+    note_path_func = function(spec)
+      -- This is equivalent to the default behavior.
+      local path = spec.dir / tostring(spec.id)
+      return path:with_suffix('.md')
+    end,
+
+    picker = {
+      name = 'telescope.nvim',
+      mappings = {
+        -- new note from query
+        new = '<C-x>',
+        -- insert link to selected note
+        insert_link = '<C-l>',
+      },
+    },
+    -- configure additional syntax highlighting / extmarks
+    -- requires `conceallevel` set to 1 or 2
+    ui = {
+      enable = true, -- set to false to disable all additional syntax features
+      update_debounce = 200, -- update delay after a text change (in milliseconds)
+      -- Define how various check-boxes are displayed
+      checkboxes = {
+        -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+        [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
+        ['x'] = { char = '', hl_group = 'ObsidianDone' },
+        ['>'] = { char = '', hl_group = 'ObsidianRightArrow' },
+        ['~'] = { char = '󰰱', hl_group = 'ObsidianTilde' },
+        -- Replace the above with this if you don't have a patched font:
+        -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+        -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+      },
+      -- Use bullet marks for non-checkbox lists.
+      bullets = { char = '•', hl_group = 'ObsidianBullet' },
+      external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
+      -- Replace the above with this if you don't have a patched font:
+      -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+      reference_text = { hl_group = 'ObsidianRefText' },
+      highlight_text = { hl_group = 'ObsidianHighlightText' },
+      tags = { hl_group = 'ObsidianTag' },
+      block_ids = { hl_group = 'ObsidianBlockID' },
+      hl_groups = {
+        -- The options are passed directly to `vim.api.nvim_set_hl()`
+        ObsidianTodo = { bold = true, fg = '#f78c6c' },
+        ObsidianDone = { bold = true, fg = '#89ddff' },
+        ObsidianRightArrow = { bold = true, fg = '#f78c6c' },
+        ObsidianTilde = { bold = true, fg = '#ff5370' },
+        ObsidianBullet = { bold = true, fg = '#89ddff' },
+        ObsidianRefText = { underline = true, fg = '#c792ea' },
+        ObsidianExtLinkIcon = { fg = '#c792ea' },
+        ObsidianTag = { italic = true, fg = '#89ddff' },
+        ObsidianBlockID = { italic = true, fg = '#89ddff' },
+        ObsidianHighlightText = { bg = '#75662e' },
+      },
+    },
+  },
+}
