d1ee8962245b3349a698ab23e2ad7181e97b85db
Tom Syroid <tsyroid@gmail.com>
2024-06-03 08:13:29 -0700

    (Add) Personalized options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,69 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- core/options.lua
+-- Nvim options
+-- See `:help vim.opt` or `:help option-list`
+
+-- Make line numbers default and relative
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = 'unnamedplus'
+
+-- Enable break indent and wrap
+vim.opt.breakindent = true
+vim.opt.wrap = true
+vim.opt.linebreak = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or
+-- more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'` and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- backspace
+vim.opt.backspace = 'indent,eol,start' -- allow backspace on indent, end of line or insert mode start position
+
+-- Preview substitutions
+vim.opt.inccommand = 'split'
+
+-- Turn off swapfile
+vim.opt.swapfile = false
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 20
+
+-- For markdown files in Obsidian
+vim.opt.conceallevel = 0
+
+-- vim: ts=2 sts=2 sw=2 et
