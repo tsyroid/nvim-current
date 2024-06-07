e5a15b0bb51856f0a1703d0597059d424280a36b
Tom Syroid <tsyroid@gmail.com>
2024-06-07 09:19:58 -0700

    (New) Add Neogit plugin

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,17 @@
+return {
+  'NeogitOrg/neogit',
+  branch = 'master',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- required
+    'sindrets/diffview.nvim', -- optional - Diff integration
+
+    -- Only one of these is needed, not both.
+    'nvim-telescope/telescope.nvim', -- optional
+  },
+  config = function()
+    local neogit = require('neogit')
+    neogit.setup({})
+
+    vim.keymap.set('n', '<leader>gs', ':Neogit<CR>', { silent = true })
+  end,
+}
