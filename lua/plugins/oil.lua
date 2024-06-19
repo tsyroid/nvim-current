06dac3710ca2d4c0eac135dc2bee58039a8ef1fb
Tom Syroid <tsyroid@gmail.com>
2024-06-19 07:00:42 -0700

    (Update) Tweak oil.lua config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,6 +5,25 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
       require('oil').setup({
+        columns = { 'icon' },
+        keymaps = {
+          ['g?'] = 'actions.show_help',
+          ['<CR>'] = 'actions.select',
+          ['<C-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
+          ['<C-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+          ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
+          ['<C-p>'] = 'actions.preview',
+          ['<C-c>'] = 'actions.close',
+          ['<C-l>'] = 'actions.refresh',
+          ['-'] = 'actions.parent',
+          ['_'] = 'actions.open_cwd',
+          ['`'] = 'actions.cd',
+          ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' },
+          ['gs'] = 'actions.change_sort',
+          ['gx'] = 'actions.open_external',
+          ['g.'] = 'actions.toggle_hidden',
+          ['g\\'] = 'actions.toggle_trash',
+        },
         default_file_explorer = true,
         delete_to_trash = true,
         skip_confirm_for_simple_edits = true,
@@ -19,7 +38,12 @@ return {
           wrap = true,
         },
       })
-      vim.keymap.set('n', '\\', '<cmd>Oil --float<CR>', { desc = 'Open Oil File Manager' })
+
+      -- Open parent directory in current window
+      vim.keymap.set('n', '--', '<cmd>Oil<CR>', { desc = 'Open parent directory' })
+
+      -- Open parent directory in floating window
+      vim.keymap.set('n', '\\', require('oil').toggle_float)
     end,
   },
 }
