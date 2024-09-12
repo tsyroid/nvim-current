20d8d912172136717556d8f91e69926ecbe45623
Tom Syroid <tsyroid@gmail.com>
2024-09-12 07:53:27 -0700

    Cleanup plugins/disabled

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,5 +2,5 @@ return {
   -- disable dashboard
   -- { 'nvimdev/dashboard-nvim', enabled = false },
   -- { 'folke/noice.nvim', enabled = false },
-  require('cmp').setup({ enabled = false }),
+  -- { 'echasnovski/mini.pairs', enabled = false },
 }
