618a4e986c02a934e7cae7c016449984ac589571
Tom Syroid <tsyroid@gmail.com>
2024-10-02 09:22:14 -0700

    (Edit) Disable catppuccin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   -- { 'nvimdev/dashboard-nvim', enabled = false },
   -- { 'folke/noice.nvim', enabled = false },
   -- { 'echasnovski/mini.pairs', enabled = false },
+  { 'catppuccin/nvim', enabled = false },
 }
